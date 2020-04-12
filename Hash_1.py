f462eef48eb27859890373b61b9766b997e8ec3f
Vegrim <63535624+Vegrim@users.noreply.github.com>
2020-04-12 19:15:45 +0900

    다시 풀어봄

--- Hash_1.py
+++ Hash_1.py
@@ -1,19 +1,13 @@
 def solution(phone_book):
     answer = True
     dict = {}
-    a = []
     for i in range(0,len(phone_book)):
-        a.append(i)
-        a[i] = str(phone_book[i])
-        dict[a[i]] = True
-    
-    for i in range(0,len(a)):
-        for j in range(1,len(a[i])):
-            b = a[i][0:j]
+        dict[phone_book[i]] = True
+        
+    for i in range(0,len(phone_book)):
+        for j in range(1,len(phone_book[i])):
+            b = phone_book[i][0:j]
             if b in dict:
                 answer = False
-                return answer
-            else :
-                answer = True
-                
+    
     return answer
